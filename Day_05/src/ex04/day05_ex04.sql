a2083166b41ccb81fe3345536bf58d08f87a378b
ilsova <nastya.batkova@mail.ru>
2024-07-24 18:43:09 +0300

    Day_05

---
+++ Day_05/src/ex04/day05_ex04.sql
@@ -0,0 +1,6 @@
+create unique index idx_menu_unique on menu(pizzeria_id, pizza_name);
+
+set enable_seqscan = off;
+explain analyse
+select * from menu
+where pizzeria_id = 1 and pizza_name = 'supreme pizza';
